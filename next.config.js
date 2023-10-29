2c545b67c0537bcf6bdea9b929a6c6f99373acb0
Catalin Miron <catalinmiron093@gmail.com>
2023-10-29 23:26:07 +0200

    first comit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {images:{
     domains:["oaidalleapiprodscus.blob.core.windows.net", "firebasestorage.googleapis.com"]
-},}
+},
+typescript:{
+    ignoreBuildErrors:true,
+},
+eslint:{ignoreDuringBuilds:true}}
 
 module.exports = nextConfig
